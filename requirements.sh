8f558a99d0652567e44c288b1f2959a869ba5696
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-24 11:05:05 +0100

    fixed requirements for cdo

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,8 @@
 
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
-    #sudo apt-get -y install wget
+    # fix for cdo on debian
+    sudo apt-get -y install libjpeg62
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     #sudo yum -y install wget
