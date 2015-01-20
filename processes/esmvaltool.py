a1bcee43274db5e4c1cba4baf6b6b9a8019de861
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-01-20 15:40:57 +0100

    refactoring esmvaltool

--- processes/esmvaltool.py
+++ processes/esmvaltool.py
@@ -5,7 +5,49 @@ from malleefowl.process import WPSProcess
 from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
-class ESMValTool(WPSProcess):
+def search(distrib, replica, limit, constraints, start_year, end_year, monitor):
+    from malleefowl.esgf.search import ESGSearch
+    esgsearch = ESGSearch(
+        url = "http://localhost:8081/esg-search",
+        distrib = distrib,
+        replica = replica,
+        latest = True,
+        monitor = monitor,
+    )
+
+    (result, summary, facet_counts) = esgsearch.search(
+        constraints = constraints,
+        query = "*:*",
+        start = "%d-01-01" % start_year,
+        end = "%d-12-31" % end_year,
+        search_type = "File",
+        limit = limit,
+        offset = 0,
+        temporal = False)
+    return result
+
+def esmvaltool():
+    from os import environ
+    archives = [path.strip() for path in environ['ESGF_ARCHIVE_ROOT'].split(':')]
+
+    from os.path import abspath, curdir, join
+    mountpoint = "%s:/data" % abspath(join(curdir, 'data'))
+    cmd = ["docker", "run", "--rm"]
+    cmd.extend([ "-v", mountpoint])
+    for archive in archives:
+        cmd.extend(["-v", "%s:%s:ro" % (archive, archive)])
+    cmd.extend(["-t", "birdhouse/esmvaltool"])
+
+    from subprocess import check_call
+    try:
+        # TODO: dont use hard coded path
+        check_call(cmd)
+    except:
+        logger.exception('docker failed')
+        import time
+        time.sleep(60)
+
+class ESMValToolProcess(WPSProcess):
     def __init__(self):
         WPSProcess.__init__(self,
             identifier = "esmvaltool",
@@ -146,8 +188,28 @@ class ESMValTool(WPSProcess):
             )
 
     def execute(self):
-        self.show_status("starting netcdf metadata retrieval", 0)
-        urls = self.search()
+        self.show_status("starting", 0)
+
+        # search
+        constraints = []
+        constraints.append( ("project", "CMIP5" ) )
+        for model in self.getInputValues(identifier='model'):
+            constraints.append( ("model", model ) )
+        for variable in self.getInputValues(identifier='variable'):
+            constraints.append( ("variable", variable ) )
+        constraints.append( ("cmor_table", self.cmor_table.getValue() ) )
+        constraints.append( ("experiment", self.experiment.getValue() ) )
+        constraints.append( ("ensemble", self.ensemble.getValue() ) )
+
+        urls = search(
+            distrib=self.distrib.getValue(),
+            replica=self.replica.getValue(),
+            limit=self.limit.getValue(),
+            constraints=constraints,
+            start_year=self.start_year.getValue(),
+            end_year=self.end_year.getValue(),
+            monitor=self.show_status
+            )
 
         from malleefowl.download import download_files
         # TODO: dont use hard coded path
@@ -183,7 +245,7 @@ class ESMValTool(WPSProcess):
 
         # run esmvaltool
         self.show_status("esmvaltool started", 10)
-        self.esmvaltool()
+        esmvaltool()
         self.show_status("esmvaltool done", 100)
 
         # output: postscript
@@ -200,60 +262,4 @@ class ESMValTool(WPSProcess):
             json.dump(obj=results, fp=fp, indent=4, sort_keys=True)
             self.summary.setValue( outfile )
 
-    def esmvaltool(self):
-        from os import environ
-        archives = [path.strip() for path in environ['ESGF_ARCHIVE_ROOT'].split(':')]
-        
-        from os.path import abspath, curdir, join
-        mountpoint = "%s:/data" % abspath(join(curdir, 'data'))
-        cmd = ["docker", "run", "--rm"]
-        cmd.extend([ "-v", mountpoint])
-        for archive in archives:
-            cmd.extend(["-v", "%s:%s:ro" % (archive, archive)])
-        cmd.extend(["-t", "birdhouse/esmvaltool"])
-        
-        from subprocess import check_call
-        try:
-            # TODO: dont use hard coded path
-            check_call(cmd)
-        except:
-            logger.exception('docker failed')
-            import time
-            time.sleep(60)
-        logger.debug('esmvaltool done')
-
-    def search(self):
-        from malleefowl.esgf.search import ESGSearch
-        esgsearch = ESGSearch(
-            url = "http://localhost:8081/esg-search",
-            distrib = self.distrib.getValue(),
-            replica = self.replica.getValue(),
-            latest = True,
-            monitor = self.show_status,
-        )
-
-        constraints = []
-        constraints.append( ("project", "CMIP5" ) )
-        for model in self.getInputValues(identifier='model'):
-            constraints.append( ("model", model ) )
-        for variable in self.getInputValues(identifier='variable'):
-            constraints.append( ("variable", variable ) )
-        constraints.append( ("cmor_table", self.cmor_table.getValue() ) )
-        constraints.append( ("experiment", self.experiment.getValue() ) )
-        constraints.append( ("ensemble", self.ensemble.getValue() ) )
-
-        (result, summary, facet_counts) = esgsearch.search(
-            constraints = constraints,
-            query = "*:*",
-            start = "%d-01-01" % self.start_year.getValue(),
-            end = "%d-12-31" % self.end_year.getValue(),
-            search_type = "File",
-            limit = self.limit.getValue(),
-            offset = 0,
-            temporal = False)
-        return result
-
-        
-
-
-
+ 
