44aa8d4830730d8e2cc7bf5b506e19b3cbc59c00
Carsten Ehbrecht <carsten@linacs.org>
2015-01-21 23:57:44 +0100

    cfchecker: show progress

--- processes/cf_checker.py
+++ processes/cf_checker.py
@@ -40,8 +40,8 @@ class CFCheckerProcess(WPSProcess):
             title="NetCDF File",
             abstract="NetCDF File",
             minOccurs=1,
-            maxOccurs=100,
-            maxmegabites=5000,
+            maxOccurs=1000,
+            maxmegabites=10000,
             formats=[{"mimeType":"application/x-netcdf"}],
             )
 
@@ -74,12 +74,13 @@ class CFCheckerProcess(WPSProcess):
         nc_files = self.getInputValues(identifier='resource')
         count = 0
         max_count = len(nc_files)
+        step = 100.0 / max_count
         for nc_file in nc_files:
             cf_report = cf_check(nc_file, version=self.cf_version.getValue())
             with open(outfile, 'a') as fp:
                 fp.write(cf_report)
                 count = count + 1
-                self.show_status("cfchecker: %d/%d" % (count, max_count), count)
+                self.show_status("cfchecker: %d/%d" % (count, max_count), int(count*step))
         self.show_status("cfchecker: done", 100)
 
 
