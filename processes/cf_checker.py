6d877e871fdc79d327e166fd3d947589337e3f8c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-01-21 15:21:58 +0100

    added cf_version paramerter to cfchecker

--- processes/cf_checker.py
+++ processes/cf_checker.py
@@ -11,11 +11,7 @@ class CFCheckerProcess(WPSProcess):
             identifier = "cfchecker",
             title = "CF Checker",
             version = "0.1",
-            abstract="""
-            The NetCDF Climate Forcast Conventions compliance checker
-            
-            http://cf-pcmdi.llnl.gov/conformance/compliance-checker/
-            """
+            abstract="The cfchecker checks NetCDF files for compliance to the CF standard."
             )
 
         self.resource = self.addComplexInput(
@@ -26,6 +22,17 @@ class CFCheckerProcess(WPSProcess):
             maxOccurs=100,
             maxmegabites=5000,
             formats=[{"mimeType":"application/x-netcdf"}],
+            )
+
+        self.cf_version = self.addLiteralInput(
+            identifier="cf_version",
+            title="CF version",
+            abstract="CF version to check against, use auto to auto-detect the file version.",
+            default="auto",
+            type=type(''),
+            minOccurs=1,
+            maxOccurs=1,
+            allowedValues=["auto", "1.6", "1.4"]
             )
 
         self.output = self.addComplexOutput(
@@ -40,11 +47,16 @@ class CFCheckerProcess(WPSProcess):
         self.show_status("starting cfchecker ...", 0)
 
         nc_file = self.getInputValues(identifier='resource')[0]
+        # TODO: maybe use local file path
+        from os import rename
+        rename(nc_file, "input.nc")
+        nc_file = "input.nc"
         from subprocess import check_output, CalledProcessError
         cmd = ["cfchecks"]
         cmd.extend( ["--cf_standard_names", "http://cfconventions.org/Data/cf-standard-names/28/src/cf-standard-name-table.xml"] )
         cmd.extend( ["--area_types", "http://cfconventions.org/Data/area-type-table/2/src/area-type-table.xml"] )
-        #cmd.extend( ["--udunits", "/home/pingu/anaconda/share/udunits/udunits2.xml"]) 
+        #cmd.extend( ["--udunits", "/home/pingu/anaconda/share/udunits/udunits2.xml"])
+        cmd.extend( ["--version", self.cf_version.getValue()] ) 
         cmd.append(nc_file)
         try:
             cf_report = check_output(cmd)
