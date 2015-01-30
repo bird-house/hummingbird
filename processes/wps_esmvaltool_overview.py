95c2a9439b77876dfdb4890c4982acd20433e2e4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-01-30 15:51:37 +0100

    esmvaltool: reduced number of models

--- processes/wps_esmvaltool_overview.py
+++ processes/wps_esmvaltool_overview.py
@@ -32,7 +32,7 @@ class ESMValToolOverviewProcess(WPSProcess):
             type=type(''),
             minOccurs=1,
             maxOccurs=10,
-            allowedValues=["ACCESS1-0", "ACCESS1-3", "CMCC-CMS", "CanCM4", "EC-EARTH", "GFDL-CM2.1", "IPSL-CM5A-LR", "IPSL-CM5A-MR", "IPSL-CM5B-LR", "MPI-ESM-LR", "MPI-ESM-MR", "MPI-ESM-P"]
+            allowedValues=["IPSL-CM5A-LR", "MPI-ESM-LR", "MPI-ESM-MR", "MPI-ESM-P"]
             )
 
         self.variable = self.addLiteralInput(
