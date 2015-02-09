741b278e10a3eaf65fd8c6b25658cb3f267a1d51
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-02-09 13:12:38 +0100

    enabled generic esmvaltool process again

--- processes/__init__.py
+++ processes/__init__.py
@@ -2,7 +2,7 @@
 __all__ = [
            "wps_cdo",
            "wps_ncmeta",
-           #"wps_esmvaltool",
+           "wps_esmvaltool",
            "wps_esmvaltool_overview",
            "wps_cfchecker",
            ]
