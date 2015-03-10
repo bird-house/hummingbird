3d9acd8f4d2862333a0d7d1a7efffae7dc11620d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-03-10 13:40:32 +0100

    removed esmvaltool processes

--- processes/__init__.py
+++ processes/__init__.py
@@ -1,11 +1,5 @@
 
 __all__ = [
            "wps_cdo",
-           "wps_ncmeta",
            "wps_cfchecker",
-           "wps_surfconplot",
-           "wps_mydiag",
-           "wps_perfmetrics",
-           "wps_taylor",
-           "wps_anncycle",
            ]
