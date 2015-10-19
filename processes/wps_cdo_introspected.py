9a4573081efa1b50e854caed67ad375e3a97be76
Florian Klemme <mail@florianklemme.de>
2015-10-19 12:21:16 +0200

    First try to generate classes from documentation.

--- processes/wps_cdo_introspected.py
+++ processes/wps_cdo_introspected.py
@@ -24,7 +24,46 @@ for op in cdo.operators:
 # Print some debug information to a local file.
 import codecs
 debugfile = codecs.open('debug_cdo_introspection.txt', 'w+', 'utf-8')
-for k, v in wps.items():
-    print >> debugfile, sorted(v)
-    print >> debugfile, k, '\n\n'
+for doc, ops in wps.items():
+    print >> debugfile, sorted(ops)
+    print >> debugfile, doc, '\n\n'
 debugfile.close()
+
+# Generate WPSs from operation documentation.
+for doc, ops, in wps.items():
+    # Get NAME from documentation.
+    import re
+    match = re.search('NAME(.*?)SYNOPSIS', doc, re.MULTILINE | re.DOTALL)
+    if match is not None:
+        name = match.group(1)
+        # Remove redundant whitespace.
+        name = re.sub('\s+', ' ', name).strip()
+
+        #print 'CDO:', name # just testing
+
+        # Add WPS for these operations.
+        # TODO: This class is being overridden all the time. Maybe fix it with 'type'?
+        class TempClassName(WPSProcess):
+            def __init__(self):
+                WPSProcess.__init__(
+                        self,
+                        identifier = 'cdo_%s' % ''.join(ops),
+                        title = 'CDO: %s' % name,
+                        version = "0.1"
+                        )
+
+                self.operator = self.addLiteralInput(
+                        identifier = 'operatior',
+                        title = 'CDO operator',
+                        type = type(''),
+                        allowedValues = sorted(ops)
+                        )
+
+                # TODO: Inputs / Outputs
+
+            def execute(self):
+                self.show_status("starting cdo operator", 10)
+
+                # TODO...
+
+                self.show_status("cdo operator done", 90)
