d03857e99456b6e8a0ffd1fafa5c47b758fc6db8
Florian Klemme <mail@florianklemme.de>
2015-10-23 02:27:29 +0200

    Disabled WPs from master branch for now.

--- processes/__init__.py
+++ processes/__init__.py
@@ -1,15 +1,16 @@
 
 __all__ = [
-           "wps_ncmeta",
-           "wps_cdo",
-           "wps_ensembles",
-           "wps_cfchecker",
-           "wps_tika",
-           "wps_hdh",
-           "wps_ioos",
-           "wps_stormtrack",
-           "wps_ncplot",
+           #"wps_ncmeta",
+           #"wps_cdo",
+           #"wps_ensembles",
+           #"wps_cfchecker",
+           #"wps_tika",
+           #"wps_hdh",
+           #"wps_ioos",
+           #"wps_stormtrack",
+           #"wps_ncplot",
            "wps_ncregrid",
            #"wps_cdo_introspected",
+           "wps_cdo_1"
            "wps_metamongo",
            ]
