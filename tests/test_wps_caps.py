4fd562f18f21318e9fb24f12b0e5b6f504fb6268
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-02 13:24:30 +0100

    using wpstestclient in cfchecker

--- tests/test_wps_caps.py
+++ tests/test_wps_caps.py
@@ -1,7 +1,7 @@
 import nose.tools
 from tests.common import WpsTestClient
 
-def test_caps():
+def test_wps_caps():
     wps = WpsTestClient()
     resp = wps.get(service='wps', request='getcapabilities')
     names = resp.xpath_text('/wps:Capabilities'
