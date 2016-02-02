2b65ff867ebb4ce91a58c612d48dcbe201b8934c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-02 14:20:38 +0100

    test wps ncplot added

---
+++ tests/test_wps_ncplot.py
@@ -0,0 +1,13 @@
+import nose.tools
+from nose.plugins.attrib import attr
+
+from tests.common import WpsTestClient, TESTDATA, assert_response_success
+
+@attr('online')
+def test_wps_simple_plot():
+    wps = WpsTestClient()
+    datainputs = "[dataset={0}]".format(TESTDATA['noaa_nc_1'])
+    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='simple_plot',
+                   datainputs=datainputs)
+    assert_response_success(resp)
+
