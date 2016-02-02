2d6132236ab9e05c98c5578489e7808e976a8942
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-02 14:10:53 +0100

    added test wps ncmeta

---
+++ tests/test_wps_ncmeta.py
@@ -0,0 +1,13 @@
+import nose.tools
+from nose.plugins.attrib import attr
+
+from tests.common import WpsTestClient, TESTDATA, assert_response_success
+
+@attr('online')
+def test_wps_ioos_cchecker():
+    wps = WpsTestClient()
+    datainputs = "[dataset={0}]".format(TESTDATA['noaa_nc_1'])
+    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='ncmeta',
+                   datainputs=datainputs)
+    assert_response_success(resp)
+
