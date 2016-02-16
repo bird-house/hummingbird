1fa483d8e27699f0e3869f94730571972f67ed7e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-16 12:56:08 +0100

    update hdh cfchecker

--- processes/wps_hdh_cfchecker.py
+++ processes/wps_hdh_cfchecker.py
@@ -28,26 +28,13 @@ def cf_check(nc_file):
         output = err.output
     return output
 
-def cordex_check(project, archive_path):
-    # generate task config
-    with open("qa.task", 'w') as fp:
-        fp.write(qa_task.format(project, archive_path))
-
-    # run checker    
-    cmd = ["qa-dkrz", "-m", "-f", "qa.task"]
-    try:
-        output = check_output(cmd, stderr=STDOUT)
-    except CalledProcessError as err:
-        logger.exception("cordex check failed!")
-        output = err.output
-    return output
 
 class CFChecker(WPSProcess):
     def __init__(self):
         WPSProcess.__init__(self,
             identifier="qa_cfchecker",
             title="QA DKRZ CF Checker",
-            version="0.5.3-0",
+            version="0.5.3-1",
             abstract="Qualtiy Assurance Tools by DKRZ: cfchecker checks NetCDF files for compliance to the CF standard.",
             statusSupported=True,
             storeSupported=True
@@ -90,54 +77,3 @@ class CFChecker(WPSProcess):
         self.status.set("cfchecker: done", 100)
 
 
-class CordexChecker(WPSProcess):
-    def __init__(self):
-        WPSProcess.__init__(self,
-            identifier = "qa_checker",
-            title = "QA DKRZ Checker",
-            version = "0.5.3-0",
-            abstract="Qualtiy Assurance Tools by DKRZ: project specific checks for cordex, cmip5, ...",
-            statusSupported=True,
-            storeSupported=True
-            )
-
-        self.project = self.addLiteralInput(
-            identifier="project",
-            title="Project",
-            default="CORDEX",
-            type=type(''),
-            minOccurs=1,
-            maxOccurs=1,
-            allowedValues=['CORDEX'])
-
-        self.archive_path = self.addLiteralInput(
-            identifier="archive_path",
-            title="Path to data archive",
-            default="",
-            type=type(''),
-            minOccurs=1,
-            maxOccurs=1,
-            )
-
-        self.output = self.addComplexOutput(
-            identifier="output",
-            title="Cordex Checker Report",
-            abstract="",
-            formats=[{"mimeType":"text/plain"}],
-            asReference=True,
-            )
-
-    def execute(self):
-        self.status.set("starting cordex checker ...", 0)
-
-        outfile = 'out.txt'
-        self.output.setValue( outfile )
-
-        report = cordex_check(self.project.getValue(), self.archive_path.getValue())
-        
-        with open(outfile, 'a') as fp:
-            fp.write(report)
-        self.status.set("cordex checker: done", 100)
-
-
-        
