278022886f86e9f92d8d3caade91af536d20320f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-03-03 18:17:39 +0100

    update ioos cchecker (2.1.0) and qa-dkrz

--- processes/wps_ioos.py
+++ processes/wps_ioos.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from contextlib import contextmanager
-from compliance_checker.runner import ComplianceChecker
+from compliance_checker.runner import ComplianceChecker, CheckSuite
+from compliance_checker import __version__ as cchecker_version
 
 from pywps.Process import WPSProcess
 
@@ -25,7 +26,7 @@ class CFCheckerProcess(WPSProcess):
         WPSProcess.__init__(self,
             identifier = "ioos_cchecker",
             title = "IOOS Compliance Checker",
-            version = "1.1.1-7",
+            version = cchecker_version,
             abstract="The IOOS Compliance Checker is a Python tool to check local/remote datasets against a variety of compliance standards.",
             metadata = [{'title': "Compliance Checker on GitHub", 'href': "https://github.com/ioos/compliance-checker"}],
             statusSupported=True,
@@ -74,28 +75,48 @@ class CFCheckerProcess(WPSProcess):
     def execute(self):
         # TODO: iterate input files ... run parallel 
         # TODO: generate html report with links to cfchecker output ...
-        # TODO: update to compliance checker 2.0
         outfile = 'out.txt'
         self.output.setValue( outfile )
         datasets = self.getInputValues(identifier='dataset')
-        checker = self.getInputValues(identifier='test')
+        checkers = self.getInputValues(identifier='test')
 
         count = 0
         max_count = len(datasets)
         step = 100.0 / max_count
 
+        check_suite = CheckSuite()
+        check_suite.load_all_available_checkers()
+
+        return_values = []
+        had_errors = []
+      
         with open(outfile, "w") as f:
             for ds in datasets:
                 f.write("\n\n####################################################################################\n")
-                f.write("checking: %s\n" % ds)
+                f.write("Running Compliance Checker on dataset: {}\n".format(ds))
                 with stdout_redirected(f):
-                    ComplianceChecker.run_checker(
+                    return_value, errors = ComplianceChecker.run_checker(
                         ds,
-                        checker_names=checker,
+                        checker_names=checkers,
                         verbose=0,
-                        criteria=self.criteria.getValue())
+                        criteria=self.criteria.getValue(),
+                        output_filename='-',
+                        output_format='text')
+                    return_values.append(return_value)
+                    had_errors.append(errors)
                 count = count + 1
                 self.status.set("checks: %d/%d" % (count, max_count), int(count*step))
+
+        if any(had_errors):
+            msg = "complince checker finshed with errors."
+            logger.warning(msg)
+            self.status.set(msg, 100)
+        elif all(return_values):
+            self.status.set("compliance checker finshed successfully", 100)
+        else:
+            msg = "complince checker finshed with errors."
+            logger.warning(msg)
+            self.status.set(msg, 100)
 
 
         
