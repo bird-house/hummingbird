d31cc386511b37dd9e35f75e6622162428bbd257
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-05 13:56:48 +0200

    update abstracts in cfchecker

--- hummingbird/processes/wps_cfchecker.py
+++ hummingbird/processes/wps_cfchecker.py
@@ -24,17 +24,24 @@ class CFCheckerProcess(WPSProcess):
     def __init__(self):
         WPSProcess.__init__(self,
             identifier = "cfchecker",
-            title = "CF Checker by BADC",
+            title = "CF Checker by NCAS Computational Modelling Services (NCAS-CMS)",
             version = "2.0.9-0",
-            abstract="The NetCDF Climate Forcast Conventions compliance checker.",
-            metadata= [ {"title": "Homepage" , "href": "https://pypi.python.org/pypi/cfchecker"} ],
+            
+            abstract="The NetCDF Climate Forcast Conventions compliance checker. This process allows you to run the compliance checker to check that the contents of a NetCDF file comply with the Climate and Forecasts (CF) Metadata Convention. The CF-checker was developed at the Hadley Centre for Climate Prediction and Research, UK Met Office by Rosalyn Hatcher. This work was supported by PRISM (PRogramme for Integrated Earth System Modelling). Development and maintenance for the CF-checker has now been taken over by the NCAS Computational Modelling Services (NCAS-CMS). If you have suggestions for improvement then please contact Rosalyn Hatcher at NCAS-CMS (r.s.hatcher@reading.ac.uk).",
+            
+            metadata= [
+                {"title": "Homepage" , "href": "https://pypi.python.org/pypi/cfchecker"},
+                {"title": "CF Conventions", "href": "http://cfconventions.org/"},
+                {"title": "Online Compliance Checker", "href": "http://cfconventions.org/compliance-checker.html"}
+                ],
             statusSupported=True,
             storeSupported=True
             )
 
         self.dataset = self.addComplexInput(
             identifier="dataset",
-            title="NetCDF File",
+            title="URL to your NetCDF File",
+            abstract="You may provide a URL or upload a NetCDF file.",
             minOccurs=1,
             maxOccurs=1000,
             maxmegabites=10000,
@@ -43,13 +50,13 @@ class CFCheckerProcess(WPSProcess):
 
         self.cf_version = self.addLiteralInput(
             identifier="cf_version",
-            title="CF version",
-            abstract="CF version to check against, use auto to auto-detect the file version.",
+            title="Check against CF version",
+            abstract="Version of CF conventions that the NetCDF file should be check against. Use auto to auto-detect the CF version.",
             default="auto",
             type=type(''),
             minOccurs=1,
             maxOccurs=1,
-            allowedValues=["auto", "1.6", "1.5", "1.4", "1.3", "1.2", "1.1"]
+            allowedValues=["auto", "1.6", "1.5", "1.4", "1.3", "1.2", "1.1", "1.0"]
             )
 
         self.output = self.addComplexOutput(
