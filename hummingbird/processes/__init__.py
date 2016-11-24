c943760cf1266f518c52c54603b518b361b02abf
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-24 18:34:57 +0100

    converte ioos cchecker to pywps-4

--- hummingbird/processes/__init__.py
+++ hummingbird/processes/__init__.py
@@ -1,7 +1,9 @@
 from .wps_ncdump import NCDump
 from .wps_spotchecker import SpotChecker
+from .wps_ioos import IOOSCChecker
 
 processes = [
     NCDump(),
     SpotChecker(),
+    IOOSCChecker(),
 ]
