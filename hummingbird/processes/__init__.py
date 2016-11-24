76dc21a06013f92aa472051076ff142659df6cdc
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-24 19:16:15 +0100

    converted cdo sinfo to pywps-4

--- hummingbird/processes/__init__.py
+++ hummingbird/processes/__init__.py
@@ -2,10 +2,12 @@ from .wps_ncdump import NCDump
 from .wps_spotchecker import SpotChecker
 from .wps_ioos import IOOSCChecker
 from .wps_cdo_op import CDOOperation
+from .wps_cdo_sinfo import CDOInfo
 
 processes = [
     NCDump(),
     SpotChecker(),
     IOOSCChecker(),
     CDOOperation(),
+    CDOInfo(),
 ]
---
+++ hummingbird/processes/wps_cdo_sinfo.py
@@ -0,0 +1,66 @@
+"""
+Processes with cdo commands
+"""
+from cdo import Cdo
+cdo_version = Cdo().version()
+
+from pywps import Process
+from pywps import LiteralInput
+from pywps import ComplexInput, ComplexOutput
+from pywps import Format, FORMATS
+from pywps.app.Common import Metadata
+
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
+
+class CDOInfo(Process):
+    """This process calls cdo sinfo on netcdf file"""
+    def __init__(self):
+        inputs = [
+            ComplexInput('dataset', 'NetCDF File',
+                         abstract='You may provide a URL or upload a NetCDF file.',
+                         metadata=[Metadata('Info')],
+                         min_occurs=1,
+                         max_occurs=100,
+                         supported_formats=[Format('application/x-netcdf')]),
+        ]
+        outputs = [
+            ComplexOutput('output', 'CDO sinfo result',
+                          abstract="CDO sinfo result",
+                          as_reference=True,
+                          supported_formats=[Format('text/plain')]),
+        ]
+
+        super(CDOInfo, self).__init__(
+            self._handler,
+            identifier="cdo_sinfo",
+            title="CDO sinfo",
+            abstract="Apply CDO sinfo on NetCDF File.",
+            version=cdo_version,
+            metadata=[
+                Metadata('Birdhouse', 'http://bird-house.github.io/'),
+                Metadata('User Guide', 'http://birdhouse-hummingbird.readthedocs.io/en/latest/'),
+                Metadata('CDO', 'https://code.zmaw.de/projects/cdo'),
+            ],
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
+        )
+
+    def _handler(self, request, response):
+        datasets = [dataset.file for dataset in request.inputs['dataset']]
+
+        cdo = Cdo()
+
+        outfile = 'out.txt'
+        with open(outfile, 'w') as fp:
+            response.outputs['output'].file = outfile
+            for ds in datasets:
+                sinfo = cdo.sinfo(input=[ds], output=outfile)
+                for line in sinfo:
+                    fp.write(line + '\n')
+                fp.write('\n\n')
+        response.update_status("cdo sinfo done", 100)
+        return response
