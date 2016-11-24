f1fcae9c881bfdaa2c83be63374624688a63c2ac
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-24 19:05:52 +0100

    converted cdo operator to pywps-4

--- hummingbird/processes/__init__.py
+++ hummingbird/processes/__init__.py
@@ -1,9 +1,11 @@
 from .wps_ncdump import NCDump
 from .wps_spotchecker import SpotChecker
 from .wps_ioos import IOOSCChecker
+from .wps_cdo_op import CDOOperation
 
 processes = [
     NCDump(),
     SpotChecker(),
     IOOSCChecker(),
+    CDOOperation(),
 ]
