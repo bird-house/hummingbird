6074a824c366f56513b73294502fe2843ea03ad8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-24 16:37:51 +0100

    update to pywps 4.x

--- hummingbird/processes/__init__.py
+++ hummingbird/processes/__init__.py
@@ -1,10 +1,2 @@
 
-__all__ = [
-    "wps_ncdump",
-    "wps_cdo",
-    "wps_ensembles",
-    "wps_cfchecker",
-    "wps_hdh_cfchecker",
-    "wps_hdh_qachecker",
-    "wps_ioos",
-    "wps_spotchecker"]
+processes = []
