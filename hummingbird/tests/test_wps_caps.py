fd7d232a6d1158066b8a248b127bf4b979b5f067
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-09 17:51:10 +0100

    enabled wps_caps test

--- hummingbird/tests/test_wps_caps.py
+++ hummingbird/tests/test_wps_caps.py
@@ -1,6 +1,27 @@
 import pytest
 
+from pywps import Service
+from pywps.tests import assert_response_success
 
-@pytest.mark.skip(reason="no way of currently testing this")
+from .common import client_for
+from hummingbird.processes import processes
+
+
 def test_wps_caps():
-    pass
+    client = client_for(Service(processes=processes))
+    resp = client.get(service='wps', request='getcapabilities', version='1.0.0')
+    names = resp.xpath_text('/wps:Capabilities'
+                            '/wps:ProcessOfferings'
+                            '/wps:Process'
+                            '/ows:Identifier')
+    assert sorted(names.split()) == [
+        'cdo_bbox',
+        'cdo_operation',
+        'cdo_sinfo',
+        'cfchecker',
+        'ensembles',
+        'ioos_cchecker',
+        'ncdump',
+        'qa_cfchecker',
+        'qa_checker',
+        'spotchecker']
