7bff51f30445d9c16a85ddd3fc5e693b74c836da
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-04 16:50:06 +0100

    update cdo=1.7.2

--- requirements.sh
+++ requirements.sh
@@ -2,8 +2,8 @@
 
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
-    # fix for cdo on debian
-    #sudo apt-get -y install libjpeg62
+    # fix for cdo/netcdf4 on debian
+    sudo apt-get -y install libjpeg8
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     #sudo yum -y install wget
