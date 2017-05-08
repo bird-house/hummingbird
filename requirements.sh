cd3a09e0524e6235a48034d6e316cdc4307be170
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-08 17:55:58 +0200

    fixed requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,7 @@
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
     # fix for cdo/netcdf4 on debian
-    sudo apt-get -y install libjpeg8
+    # sudo apt-get -y install libjpeg8
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     #sudo yum -y install wget
