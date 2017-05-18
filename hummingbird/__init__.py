2c5c091647b5952921d0cfcb18979cb9a90e06b8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-18 17:06:59 +0200

    added wsgi application

--- hummingbird/__init__.py
+++ hummingbird/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.5.5'
+import os
+from pywps.app.Service import Service
+
+from hummingbird.processes import processes
+
+__version__ = "0.5.5"
+
+
+def application(environ, start_response):
+    app = Service(processes, [os.environ.get('PYWPS_CFG')])
+    return app(environ, start_response)
