430500a81720fe894e1093dee0e73da4d75bc1fe
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-07-05 16:48:48 +0200

    added test for fix_filename

--- hummingbird/tests/test_utils.py
+++ hummingbird/tests/test_utils.py
@@ -1,4 +1,9 @@
 from hummingbird.utils import output_filename
+from hummingbird.utils import fix_filename
+
+
+def test_fix_filename():
+    assert fix_filename('test.nc') == 'test.nc'
 
 
 def test_output_filename():
