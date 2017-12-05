f97e3a3d33c145e44836b857dfe3ec34e261f8ac
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-12-05 13:14:03 +0100

    using pytest skipif

--- hummingbird/tests/test_wps_compliance_checker.py
+++ hummingbird/tests/test_wps_compliance_checker.py
@@ -2,11 +2,15 @@ import pytest
 from pywps import Service
 from pywps.tests import assert_response_success
 
+import requests
+
 from .common import TESTDATA, client_for
 from hummingbird.processes.wps_compliance_checker import CChecker
 
 
-# @pytest.mark.skip(reason="no way of currently testing this")
+@pytest.mark.skipif(
+    requests.head(TESTDATA['noaa_dap_1']).ok is False,
+    reason="website unavailable")
 @pytest.mark.online
 def test_wps_cchecker():
     client = client_for(Service(processes=[CChecker()]))
